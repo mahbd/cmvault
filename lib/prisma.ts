da4beaef71be42cc6f9d69fbd03088950d494240
Mahmudul Alam <mahmudula2000@gmail.com>
2025-12-05 18:57:10 +0600

    fixed migration issue from prisma 5 to prisma 7

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,16 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client';
+import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma = globalForPrisma.prisma || new PrismaClient({
-    datasourceUrl: process.env.DATABASE_URL
-})
+const adapter = new PrismaBetterSqlite3({
+    url: process.env.DATABASE_URL ?? 'file:./prisma/dev.db'
+});
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+export const prisma =
+    globalForPrisma.prisma ||
+    new PrismaClient({
+        adapter,
+    });
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
