2a7b1f7c7ac5a6c68617781cd7c6dfe8d0ef1381
Mahmudul Alam <mahmudula2000@gmail.com>
2025-12-16 09:40:01 +0600

    feat: Migrate database provider from SQLite to PostgreSQL.

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,11 +1,13 @@
 import { PrismaClient } from '@prisma/client';
-import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
+import { Pool } from 'pg';
+import { PrismaPg } from '@prisma/adapter-pg';
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-const adapter = new PrismaBetterSqlite3({
-    url: process.env.DATABASE_URL ?? 'file:./prisma/dev.db'
-});
+const connectionString = `${process.env.DATABASE_URL}`;
+
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
 
 export const prisma =
     globalForPrisma.prisma ||
